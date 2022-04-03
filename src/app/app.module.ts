ebda21ec524b16aa27a5d874f8cde970862fa7d4
nathan <maerna1@gmail.com>
2022-04-03 13:38:03 +0200

    set header html/css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IndexComponent } from './index/index.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
