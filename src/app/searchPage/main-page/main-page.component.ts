6a4ddfa4d1554a105434816ce2c3382cf557ab6e
nathan <maerna1@gmail.com>
2022-04-07 10:59:57 +0200

    set up card of provider

--- src/app/searchPage/main-page/main-page.component.ts
+++ src/app/searchPage/main-page/main-page.component.ts
@@ -6,7 +6,6 @@ import { Router } from '@angular/router';
 
 window.addEventListener('resize', function(event){
   const container = document.getElementById("containerSelector");
-  console.log("coucou")
   if(window.innerWidth < 1436){
     document.getElementById("SearchInput")!.style.left = "380px";
     container!.style.marginRight = "0";
@@ -37,11 +36,14 @@ export class MainPageComponent implements OnInit {
   private updateCSSOfComponentSelector() {
 
     document.getElementById("containerSelector")!.style.maxWidth = "1000px";
+    document.getElementById("parentSelector")!.style.boxShadow = "2px 1px 10px rgb(0,0,0, 0.2)";
     document.getElementById("containerSelector")!.style.marginRight = "0";
     document.getElementById("SearchInput")!.style.left = "250px";
     document.getElementById("CitySelector")!.style.position = "relative";
     document.getElementById("CitySelector")!.style.left = "6vw";
-
+    let htmlDocument = document.body.parentNode
+    // @ts-ignore
+    htmlDocument.style.backgroundColor = "white"
   }
 
 }
