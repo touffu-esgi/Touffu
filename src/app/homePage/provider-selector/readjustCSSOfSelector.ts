62aa8fcd46519726183a97919a4e03916b469121
nathan <maerna1@gmail.com>
2022-04-07 11:41:29 +0200

    move ts css update in other file

---
+++ src/app/homePage/provider-selector/readjustCSSOfSelector.ts
@@ -0,0 +1,28 @@
+export function resize(event: Event){
+  const container = document.getElementById("containerSelector");
+  if(window.innerWidth < 1436){
+    document.getElementById("SearchInput")!.style.left = "380px";
+    container!.style.marginRight = "0";
+    container!.style.position = "relative";
+    container!.style.left = "25%";
+    container!.style.width = "1000px";
+  }else{
+    container!.style.left = "";
+    container!.style.maxWidth = "1000px";
+  }
+}
+
+export function updateCSSOfComponentSelector() {
+  document.getElementById("containerSelector")!.style.minWidth = "887px";
+  document.getElementById("containerSelector")!.style.marginTop = "150px";
+  document.getElementById("containerSelector")!.style.marginRight = "0";
+  document.getElementById("parentSelector")!.style.boxShadow = "2px 1px 10px rgb(0,0,0, 0.2)";
+  document.getElementById("SearchInput")!.style.left = "270px";
+  document.getElementById("CitySelector")!.style.position = "relative";
+  document.getElementById("CitySelector")!.style.left = "6vw";
+  document.getElementById("animal")!.style.boxShadow = "2px 1px 10px rgb(0,0,0, 0.2)";
+
+  let htmlDocument = document.body.parentNode
+  // @ts-ignore
+  htmlDocument.style.backgroundColor = "white"
+}
--- src/app/searchPage/main-page/main-page.component.ts
+++ src/app/searchPage/main-page/main-page.component.ts
@@ -1,23 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import {resize, updateCSSOfComponentSelector} from "../../homePage/provider-selector/readjustCSSOfSelector";
 
-
-
-
-window.addEventListener('resize', function(event){
-  const container = document.getElementById("containerSelector");
-  if(window.innerWidth < 1436){
-    document.getElementById("SearchInput")!.style.left = "380px";
-    container!.style.marginRight = "0";
-    container!.style.position = "relative";
-    container!.style.left = "25%";
-    container!.style.width = "1000px";
-  }else{
-    container!.style.left = "";
-    container!.style.maxWidth = "1000px";
-  }
-});
-
+window.addEventListener('resize', resize);
 
 @Component({
   selector: 'app-main-page',
@@ -30,20 +15,9 @@ export class MainPageComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.updateCSSOfComponentSelector()
+    updateCSSOfComponentSelector();
   }
 
-  private updateCSSOfComponentSelector() {
 
-    document.getElementById("containerSelector")!.style.maxWidth = "1000px";
-    document.getElementById("parentSelector")!.style.boxShadow = "2px 1px 10px rgb(0,0,0, 0.2)";
-    document.getElementById("containerSelector")!.style.marginRight = "0";
-    document.getElementById("SearchInput")!.style.left = "250px";
-    document.getElementById("CitySelector")!.style.position = "relative";
-    document.getElementById("CitySelector")!.style.left = "6vw";
-    let htmlDocument = document.body.parentNode
-    // @ts-ignore
-    htmlDocument.style.backgroundColor = "white"
-  }
 
 }
