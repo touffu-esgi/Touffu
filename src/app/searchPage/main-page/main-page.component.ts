4e25a8a4940a1fb1df1c09d59088d698e90d3671
nathan <maerna1@gmail.com>
2022-04-07 10:03:35 +0200

    override media querry of selector in search page in ts

--- src/app/searchPage/main-page/main-page.component.ts
+++ src/app/searchPage/main-page/main-page.component.ts
@@ -3,6 +3,23 @@ import { Router } from '@angular/router';
 
 
 
+
+window.addEventListener('resize', function(event){
+  const container = document.getElementById("containerSelector");
+  console.log("coucou")
+  if(window.innerWidth < 1436){
+    document.getElementById("SearchInput")!.style.left = "380px";
+    container!.style.marginRight = "0";
+    container!.style.position = "relative";
+    container!.style.left = "25%";
+    container!.style.width = "1000px";
+  }else{
+    container!.style.left = "";
+    container!.style.maxWidth = "1000px";
+  }
+});
+
+
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
@@ -20,7 +37,8 @@ export class MainPageComponent implements OnInit {
   private updateCSSOfComponentSelector() {
 
     document.getElementById("containerSelector")!.style.maxWidth = "1000px";
-    document.getElementById("SearchInput")!.style.left = "15vw";
+    document.getElementById("containerSelector")!.style.marginRight = "0";
+    document.getElementById("SearchInput")!.style.left = "250px";
     document.getElementById("CitySelector")!.style.position = "relative";
     document.getElementById("CitySelector")!.style.left = "6vw";
 
