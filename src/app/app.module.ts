f906ccc0ef7b99047d45b6e969ffd8518053e9ce
Nigth <omnes.theo@gmail.com>
2022-06-03 22:32:49 +0200

    separate header in two components : icon & actions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { HeaderIconComponent } from './header/header-icon/header-icon.component';
+import { HeaderActionsComponent } from './header/header-actions/header-actions.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HeaderIconComponent,
+    HeaderActionsComponent
   ],
   imports: [
     BrowserModule,
