4c75b43fa4cf058153efa6762df64eadfff727ad
nathan <maerna1@gmail.com>
2022-06-13 23:04:18 +0200

    add searchPage route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import {ProviderDescriptionPageComponent} from "./provider-description-page/provider-description-page.component";
 import {SearchProviderPageComponent} from "./search-provider-page/search-provider-page.component";
 import {ProviderData} from "./domaine/providerData";
+import {HomeComponent} from "./homePage/home/home.component";
 
 const routes: Routes = [
   {path: 'description-component', component: ProviderDescriptionPageComponent, data: ProviderData},
-  {path: '', component: SearchProviderPageComponent},
+  {path: 'search-page', component: SearchProviderPageComponent},
+  {path: '', component: HomeComponent},
 ];
 
 @NgModule({
