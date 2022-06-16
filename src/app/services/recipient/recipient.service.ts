42a99b3ff52bc0cb3ca921c0151bd949aaf682b4
nathan <maerna1@gmail.com>
2022-06-16 10:31:55 +0200

    refactor httpUrils

--- src/app/services/recipient/recipient.service.ts
+++ src/app/services/recipient/recipient.service.ts
@@ -2,17 +2,18 @@ import { Injectable } from '@angular/core';
 import {RecipientServiceInterface} from "./recipient.service.interface";
 import {Recipient} from "../../domaine/recipient/recipient";
 import {HttpClient} from "@angular/common/http";
+import {HttpUtils} from "../../utils/http.utils";
 
 @Injectable({
   providedIn: 'root'
 })
 export class RecipientService implements RecipientServiceInterface{
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private httpUtils: HttpUtils) { }
 
   signUp(recipient: Recipient): Promise<Boolean> {
 
-    console.log()
+    console.log(this.httpUtils.fullUrl())
     return Promise.resolve(false);
   }
 }
--- src/app/utils/http.utils.ts
+++ src/app/utils/http.utils.ts
@@ -1,3 +1,10 @@
-export abstract class URL{
+import {Injectable} from "@angular/core";
 
+@Injectable({
+  providedIn: 'root'
+})
+export abstract class HttpUtils{
+  fullUrl(port: string = "3000"): string {
+    return `${document.location.protocol}://${document.location.hostname}:${port}`;
+  }
 }
