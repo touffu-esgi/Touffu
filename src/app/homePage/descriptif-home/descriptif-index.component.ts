9bf3bf8ed6312ed73033aa49eebe0268cf68232a
nathan <maerna1@gmail.com>
2022-06-19 21:55:26 +0200

    refont home page scss

--- src/app/homePage/descriptif-home/descriptif-index.component.ts
+++ src/app/homePage/descriptif-home/descriptif-index.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-descriptif-index',
   templateUrl: './descriptif-index.component.html',
-  styleUrls: ['./descriptif-index.component.css']
+  styleUrls: ['./descriptif-index.component.scss']
 })
 export class DescriptifIndexComponent implements OnInit {
 
