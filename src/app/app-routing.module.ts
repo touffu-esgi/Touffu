9dbd2f9a5e74e54456e4a6a4c225872f6ebd16ec
nathan <maerna1@gmail.com>
2022-06-22 22:51:01 +0200

    add auth guard on route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,15 +8,36 @@ import {MessagePageComponent} from "./message-page/message-page.component";
 import { ConnectionPageComponent } from './connection-page/connection-page.component';
 import { ListAnimalPageComponent } from './list-animal-page/list-animal-page.component';
 import { AddAnimalPageComponent } from './add-animal-page/add-animal-page.component';
+import { AuthGuard } from './auth-guard';
 
 const routes: Routes = [
-  {path: 'description-component', component: ProviderDescriptionPageComponent, data: ProviderData},
-  {path: 'search-page', component: SearchProviderPageComponent},
-  {path: 'message', component: MessagePageComponent},
-  {path: 'connection', component: ConnectionPageComponent},
-  {path: 'list-animal', component: ListAnimalPageComponent},
-  {path: 'add-animal', component: AddAnimalPageComponent},
+  {
+    path: 'description-component',
+    component: ProviderDescriptionPageComponent,
+    data: ProviderData},
+  {
+    path: 'search-page',
+    component: SearchProviderPageComponent},
+  {
+    path: 'message',
+    component: MessagePageComponent,
+    canActivate: [AuthGuard]},
+  {
+    path: 'connection',
+    component: ConnectionPageComponent
+  },
+  {
+    path: 'list-animal',
+    component: ListAnimalPageComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'add-animal',
+    component: AddAnimalPageComponent,
+    canActivate: [AuthGuard]
+  },
   {path: '', component: HomeComponent},
+  {path: '**', component: HomeComponent},
 ];
 
 @NgModule({
