00ad48e0d7b3b09038f941e6acf942a1f2534472
nathan <maerna1@gmail.com>
2022-06-27 12:35:24 +0200

    pass provider id to the child component

--- src/app/agreement-page/start-date/start-date.component.ts
+++ src/app/agreement-page/start-date/start-date.component.ts
@@ -1,7 +1,8 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output, Provider } from '@angular/core';
 import { AvailabilityService } from '../../services/availability/availability.service';
 import { Availability } from '../../domaine/availability/availability';
 import { Agreement } from '../../domaine/agreement/agreement';
+import { ProviderData } from '../../domaine/providerData';
 
 @Component({
   selector: 'app-start-date',
@@ -11,13 +12,14 @@ import { Agreement } from '../../domaine/agreement/agreement';
 export class StartDateComponent implements OnInit {
   @Output() weekly = new EventEmitter<Availability[]>();
   @Input() agreement?: Agreement;
+  @Input() providerId?: string;
   constructor(private availabilityService: AvailabilityService) {}
 
   ngOnInit(): void {
   }
 
   getWeeklyRecurrence(startDate: HTMLInputElement) {
-    this.availabilityService.getWeeklyAvailability("1", startDate.value.toString()).subscribe(weeklyAvailability => {
+    this.availabilityService.getWeeklyAvailability(this.providerId!, startDate.value.toString()).subscribe(weeklyAvailability => {
       this.weekly.emit(weeklyAvailability);
     });
   }
