b1b0f769e950ca644140e8471ab886dbf9b1fa60
nathan <maerna1@gmail.com>
2022-06-28 10:10:36 +0200

    add verification if user complete all fields

--- src/app/services/recommandation/recommandation.service.ts
+++ src/app/services/recommandation/recommandation.service.ts
@@ -2,13 +2,14 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Recommendation } from '../../domaine/recommendation/recommendation';
+import { HttpUtils } from '../../utils/http.utils';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RecommandationService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private httpUtils: HttpUtils) { }
 
 
   addRecommendation(recommendation:Recommendation): void{
@@ -19,11 +20,11 @@ export class RecommandationService {
       "score": recommendation.score,
       "dateReview": new Date()
     })
-    this.http.post("http://localhost:3000/recommendation", body, {headers: {'Content-Type': 'application/json'}}).subscribe()
+    this.http.post(this.httpUtils.fullUrl() + "/recommendation", body, {headers: {'Content-Type': 'application/json'}}).subscribe()
   }
 
   getRecommendations(providerId: string): Observable<Recommendation[]>{
-    return this.http.get<Recommendation[]>(`http://localhost:3000/recommendation/${providerId}`)
+    return this.http.get<Recommendation[]>(this.httpUtils.fullUrl() + `/recommendation/${providerId}`)
   }
 
 }
