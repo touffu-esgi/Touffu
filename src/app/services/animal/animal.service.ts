1113b0155519b7cc5dcc2237c05117084d693942
nathan <maerna1@gmail.com>
2022-06-28 10:18:54 +0200

    remove localhost

--- src/app/services/animal/animal.service.ts
+++ src/app/services/animal/animal.service.ts
@@ -3,21 +3,22 @@ import { HttpClient } from '@angular/common/http';
 import { Animal } from '../../homePage/animal/animal';
 import { Observable } from 'rxjs';
 import { AuthService } from '../auth/auth.service';
+import { HttpUtils } from '../../utils/http.utils';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AnimalService {
 
-  constructor(private http: HttpClient, private auth: AuthService) {}
+  constructor(private http: HttpClient, private auth: AuthService, private httpUtils: HttpUtils) {}
 
   getAnimalsByRecipientId(recipientId: string): Observable<Animal[]>{
-    return this.http.get<Animal[]>(`http://localhost:3000/animals/${recipientId}`);
+    return this.http.get<Animal[]>(this.httpUtils.fullUrl() + `/animals/${recipientId}`);
   }
 
   addAnimal(animal: Animal): Observable<string>{
     animal.recipientId = this.auth.user!.id!;
     const body = JSON.stringify(animal);
-    return this.http.post<string>(`http://localhost:3000/animals`, body, {headers: {'Content-Type': 'application/json'}});
+    return this.http.post<string>(this.httpUtils.fullUrl() +  `/animals`, body, {headers: {'Content-Type': 'application/json'}});
   }
 }
