cd1272da4f9f7d9c00f597027ea328f792fe49ee
nathan <maerna1@gmail.com>
2022-07-05 13:42:10 +0200

    router redirection

--- src/app/add-animal-page/add-animal-page.component.ts
+++ src/app/add-animal-page/add-animal-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Animal } from '../homePage/animal/animal';
 import { AnimalService } from '../services/animal/animal.service';
 import { Observable, Subscription } from 'rxjs';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-add-animal-page',
   templateUrl: './add-animal-page.component.html',
@@ -11,13 +12,13 @@ export class AddAnimalPageComponent implements OnInit {
   animal: Animal = new Animal("", "", "");
   private animalAddSubscribe?: Subscription;
 
-  constructor(private animalService: AnimalService) { }
+  constructor(private animalService: AnimalService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
   addAnimal() {
-    this.animalAddSubscribe = this.animalService.addAnimal(this.animal).subscribe(() => window.location.replace("/list-animal"));
+    this.animalAddSubscribe = this.animalService.addAnimal(this.animal).subscribe(() => this.router.navigate(['/list-animal']));
   }
 
   ngDestroy(){
