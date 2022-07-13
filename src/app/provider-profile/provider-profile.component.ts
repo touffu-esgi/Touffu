c5f2bba1901d80a0b541c73fcc69431619c2ad84
nathan <maerna1@gmail.com>
2022-07-13 11:38:08 +0200

    add agreement detail in provider profile"

--- src/app/provider-profile/provider-profile.component.ts
+++ src/app/provider-profile/provider-profile.component.ts
@@ -4,6 +4,7 @@ import { Bill } from '../domaine/bill/bill';
 import { AuthService } from '../services/auth/auth.service';
 import { ProviderData } from '../domaine/providerData';
 import { ProviderService } from '../services/provider/provider.service';
+import { Agreement } from '../domaine/agreement/agreement';
 
 @Component({
   selector: 'app-provider-profile',
@@ -13,6 +14,10 @@ import { ProviderService } from '../services/provider/provider.service';
 export class ProviderProfileComponent implements OnInit {
   bills: Bill[] = []
   provider?: ProviderData;
+  agreements: Agreement[] = [];
+  agreementToDisplay?: Agreement;
+  displayList: boolean = true;
+
   constructor(
     private billService: BillService,
     private authService: AuthService,
@@ -39,5 +44,10 @@ export class ProviderProfileComponent implements OnInit {
     this.providerService.getOneProviderByUrl(this.authService?.user?.userReference!).subscribe(provider => {
       this.provider = provider
     })
+  }
+
+  displayAgreement(agreement: Agreement) {
+    this.agreementToDisplay = agreement;
+    this.displayList = false;
   }
 }
